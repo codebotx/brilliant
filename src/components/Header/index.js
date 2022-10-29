c6bb31c2be4a1556ba8a9db1c18ea57ef448c2f2
codebotx <anubhabr50@gmail.com>
2022-10-29 13:43:21 +0530

    modified logo and customized navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,54 +3,38 @@ import { Button } from "@material-ui/core";
 
 import "./style.css";
 import logo from "../../assets/logo.svg";
-
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 export default function Header() {
   return (
-    <div className="header">
-      <div className="header-heading">
-        <img style={{ width: "40px" }} src={logo}></img>{" "}
-        <span style={{ padding: "0px 10px" }}>BRILLIANT</span>
-      </div>
-      <div className="header-links">
-        <div>TODAY</div>
-        <div>COURSES</div>
-        <div>PRACTICE</div>
-      </div>
-      <div
-        className="header-button-wrapper"
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          width: "380px",
-          marginLeft: "auto",
-          marginRight: "5%",
-        }}
-      >
-        <Button
-          className="header-button"
-          style={{
-            width: "182px",
-            borderColor: "#049BE5",
-            height: "32px",
-            color: "#049BE5",
-          }}
-          variant="outlined"
-        >
-          L<span style={{ textTransform: "lowercase" }}>og in</span>
-        </Button>
-        <Button
-          className="header-button"
-          style={{
-            width: "182px",
-            backgroundColor: "#049BE5",
-            height: "32px",
-            color: "white",
-          }}
-          variant="contained"
-        >
-          S<span style={{ textTransform: "lowercase" }}>ign up</span>
-        </Button>
-      </div>
-    </div>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <div className="header-heading">
+          <img style={{ width: "40px" }} src={logo}></img>{" "}
+          <span style={{ padding: "0px 10px" }}></span>
+        </div>
+        <Navbar.Brand href="#home"> BASS ACADEMY</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Courses" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Course X</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Course Y
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Course Z</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Testimonials
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
